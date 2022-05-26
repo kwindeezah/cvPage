bc96e32ff3ba717331ac42ea7b824c5de12f92cf
MACBOOK 2020 <deejaykas@Deezahs-MacBook-Pro.local>
2022-05-26 14:02:50 +0100

    MyFirstCommit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My CV</title>
+    <link href="cv-style.css" rel="stylesheet">
+</head>
+<body class="container">
+    <div class="header" >
+        <nav>
+            <h4 class="name">Hadizat Oluwaseun Abdulhakieem</h4>
+            <ul class="navlinks">
+                <li><a href="">ABOUT ME</a></li>
+                <li><a href="">RESUME</a></li>
+                <li><a href="">PORTFOLIO</a></li>
+                <li><a href="">BLOG</a></li>
+                <li class="btn">CONTACT ME</li>
+            </ul>
+        </nav>
+    </div>
+    <div class="header2">
+        <nav>
+            <h2 class="resume">RESUME</h2>
+            <ul class="nav">
+                <li><a href="" id="edit">EDIT RESUME</a></li>
+            </ul>
+        </nav>
+    </div>
+    <section class="section1">
+        <h2 class="prof">PROFESSIONAL DETAILS</h2>
+        <img class="img" src="./img.jpeg" width="120" height="auto">
+        <div class="p">
+        <p><strong>NAME</strong><span>Hadizat Abdulhakieem</span></p><br>
+        <p><strong>AGE</strong><span>24 Years</span></p><br>
+        <p><strong>LOCATION</strong><span>Lagos, Nigeria</span></p><br>
+        <p><strong>EXPERIENCE</strong><span>1 Year</span></p><br>
+        <p><strong>DEGREE</strong><span>HND</span></p>
+        </div>
+    </section>
+    <section class="section2">
+        <h2 class="abt">ABOUT ME</h2>
+        <br>
+        <hr>
+<div class="para">
+        <p><strong>NAME</strong><span>Hadizat Abdulhakieem</span><strong> CARRER LEVEL</strong><span>Beginner Level</span></p><br>
+        <p><strong>AGE</strong><span>24 Years</span><strong> PHONE</strong><span>+234 809 549 3097</span></p><br>
+        <p><strong>LOCATION</strong><span>Lagos, Nigeria</span><strong> EMAIL</strong><a href="hadizathmotunrayo@gmail.com"><span style="color:blanchedalmond;";>hadizathmotunrayo@gmail.com</span></a></p><br>
+        <p><strong>EXPERIENCE</strong><span>1 Year</span><strong> DEGREE</strong><span>HND</span></p><br>
+</div>
+    </section>
+</body>
+</html>
