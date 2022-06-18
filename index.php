86808e90db4951d66b14257e606bc3079bcdc4fb
Hadizat Oluwaseun <hadizathmotunrayo@gmail.com>
2022-06-18 20:00:47 +0100

    commit

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body class="container">
     <div class="header" >
         <nav>
-            <h4 class="name">Hadizat Oluwaseun Abdulhakieem</h4>
+            <h4 class="name">Hadizat Abdulhakieem</h4>
             <ul class="navlinks">
                 <li><a href="">ABOUT ME</a></li>
                 <li><a href="">RESUME</a></li>
@@ -28,28 +28,60 @@
             </ul>
         </nav>
     </div>
-    <!-- new change -->
-    <section class="section1">
+    
+    <div class="row">
+    <section class="col-4 col-s-4">
         <h2 class="prof">PROFESSIONAL DETAILS</h2>
-        <img class="img" src="./img.jpeg" width="120" height="auto">
-        <div class="p">
-        <p><strong>NAME</strong><span>Hadizat Abdulhakieem</span></p><br>
-        <p><strong>AGE</strong><span>24 Years</span></p><br>
-        <p><strong>LOCATION</strong><span>Lagos, Nigeria</span></p><br>
-        <p><strong>EXPERIENCE</strong><span>1 Year</span></p><br>
-        <p><strong>DEGREE</strong><span>HND</span></p>
+        <img class="img" src="./img.jpeg" width="150" height="auto">
+        <div class="bold">
+        <p><strong>NAME</strong></p><br>
+        <p><strong>AGE</strong></p><br>
+        <p><strong>LOCATION</strong></p><br>
+        <p><strong>EXPERIENCE</strong></p><br>
+        <p><strong>DEGREE</strong></p>
+        </div>
+        <div class="span">
+        <p><span>Hadizat Abdulhakieem</span></p><br>
+        <p><span>24 Years</span></p><br>
+        <p><span>Lagos, Nigeria</span></p><br>
+        <p><span>1 Year</span></p><br>
+        <p><span>HND</span></p><br>
         </div>
     </section>
-    <section class="section2">
+    <section class="col-8 col-s-12">
         <h2 class="abt">ABOUT ME</h2>
-        <br>
         <hr>
-<div class="para">
-        <p><strong>NAME</strong><span>Hadizat Abdulhakieem</span><strong> CARRER LEVEL</strong><span>Beginner Level</span></p><br>
-        <p><strong>AGE</strong><span>24 Years</span><strong> PHONE</strong><span>+234 809 549 3097</span></p><br>
-        <p><strong>LOCATION</strong><span>Lagos, Nigeria</span><strong> EMAIL</strong><a href="hadizathmotunrayo@gmail.com"><span style="color:blanchedalmond;";>hadizathmotunrayo@gmail.com</span></a></p><br>
-        <p><strong>EXPERIENCE</strong><span>1 Year</span><strong> DEGREE</strong><span>HND</span></p><br>
-</div>
+<div class="bold2">
+        <p><strong>NAME</strong></p><br>
+        <p><strong>AGE</strong></p><br>
+        <p><strong>LOCATION</strong></p><br>
+        <p><strong>EXPERIENCE</strong></p>
+    </div>
+        <div class="span2">
+        <p><span>Hadizat Abdulhakieem</span></p><br>
+        <p><span>24 Years</span></p><br>
+        <p><span>Lagos, Nigeria</span></p><br>
+        <p><span>1 Year</span></p>
+        </div>
+        <div class="bold3">
+            <p><strong> CARRER LEVEL</strong></p><br>
+            <p><strong> PHONE</strong></p><br>
+            <p><strong> EMAIL</strong></p><br>
+            <p><strong> DEGREE</strong></p>
+        </div>
+        <div class="span3">
+            <p><span>Beginner Level</span></p><br>
+            <p><span>+234 809 549 3097</span></p><br>
+            <p><a href="hadizathmotunrayo@gmail.com"><span style="color:blanchedalmond;";>hadizathmotunrayo@gmail.com</span></a></p><br>
+            <p><span>HND</span></p>
+        </div>
+
+        <div class="write">
+            <h1 class="h1">I'm a PHP Developer</h1><br>
+            <hr><br>
+            <p class="pas">I'm very passionate about software development, this says much about why I transitioned from Digital Marketing to Backend Development.</p>
+        </div>
     </section>
+    </div>
 </body>
 </html>
